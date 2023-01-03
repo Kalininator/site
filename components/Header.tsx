575f2823efefa7b31175555ccb4c3c2b6ed83bd2
Alexander Kalinin <alex@kalinin.uk>
2023-01-03 20:08:07 +0000

    header tailwind

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,21 @@
 import Link from "next/link";
 
-import styles from "../styles/Header.module.css";
 const menuItems = [{ label: `Home`, url: `/` }];
 
 const Header = () => {
   return (
-    <header className={styles.site__header}>
-      <div className={`${styles.nav__area} ${styles.inner}`}>
-        <Link href="/" className={styles.logo}>
+    <header className="shadow">
+      <div className="mx-auto flex h-14 max-w-screen-md flex-wrap items-center justify-between px-2">
+        <Link href="/" className="text-2xl">
           Alex Kalinin
         </Link>
         <nav>
-          <ul className={styles.menus}>
+          <ul className="flex list-none">
             {menuItems.map(({ url, label }, index) => (
-              <li className={styles.menu__items} key={index}>
+              <li
+                className="relative block text-left text-base hover:underline"
+                key={index}
+              >
                 <Link href={url}>{label}</Link>
               </li>
             ))}
