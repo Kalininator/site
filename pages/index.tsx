d2a1d887e110f55409f40f845ea4f17649e4e956
Alexander Kalinin <alex@kalinin.uk>
2023-01-04 20:23:21 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home({ postsData }: { postsData: any }) {
             const { slug, title, date, description, readingTime } = post;
 
             return (
-              <li className="mb-8 list-none last:mb-0 hover:shadow" key={slug}>
+              <li className="mb-4 list-none last:mb-0 hover:shadow" key={slug}>
                 <Link href={`/blog/${slug}`} className="">
                   <article className="rounded-xl border border-slate-200 p-5">
                     <h2 className="mb-2 text-2xl font-semibold leading-normal">
