edec58255d14a95924c28ec5536cb9aa850b9d3b
Alexander Kalinin <alex@kalinin.uk>
2023-01-05 00:30:28 +0000

    footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,26 @@
 import { MdRssFeed } from "react-icons/md";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="mx-auto flex max-w-screen-md items-center justify-between border-t border-violet-200 px-4 py-2 text-violet-700 dark:border-violet-900">
+      <div className="mx-auto flex max-w-screen-md items-center justify-between space-x-4 border-t border-violet-200 px-4 py-2 text-violet-700 dark:border-violet-900">
         <p>&copy; {new Date().getFullYear().toString()} by Alex Kalinin</p>
+        <div className="grow"></div>
+        <a
+          href="https://github.com/kalininator"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <BsGithub size="30px" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/kalininator/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <BsLinkedin color="#0072b1" size="30px" />
+        </a>
         <a
           href="http://www.kalinin.uk/rss.xml"
           rel="noreferrer"
