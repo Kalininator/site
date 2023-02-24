77b45b41afc1dd8738b6b826ae41ba62b80e5cfe
Alex Kalinin <alex@kalinin.uk>
2023-02-24 18:07:52 +0000

    add disqus (#7)

---
+++ components/DisqusComments.tsx
@@ -0,0 +1,23 @@
+import { DiscussionEmbed } from "disqus-react";
+const DisqusComments = ({
+  post,
+}: {
+  post: {
+    id: string;
+    title: string;
+    slug: string;
+  };
+}) => {
+  const disqusShortname = "kalinin";
+  const disqusConfig = {
+    url: `https://www.kalinin.uk/posts/${post.slug}`,
+    identifier: post.id, // Single post id
+    title: post.title, // Single post title
+  };
+  return (
+    <div className="mt-20">
+      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+    </div>
+  );
+};
+export default DisqusComments;
