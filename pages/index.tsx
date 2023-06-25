6aeaa7245c63c560039867a9fe3d23f65b0f614d
Alexander Kalinin <alex@kalinin.uk>
2023-06-26 00:35:51 +0700

    smaller bg image?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import bg from "../public/background-dim.jpg";
+import bg from "../public/background-dim-small.jpg";
 
 export default function Home() {
   return (
