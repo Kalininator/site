dd1e6a31a7ea4ca1c943d1852730119711cc9af7
Alexander Kalinin <alex@kalinin.uk>
2023-09-01 18:38:07 +0100

    sitemap, robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kalinin.dev',
+  site: 'https://www.kalinin.dev',
   integrations: [mdx(), sitemap(), tailwind()]
 })
